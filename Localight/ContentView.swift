a8d29f8f2adb84b06b05a99e6fc8bee1544f77b1
Timo Köthe <timo.info@t-online.de>
2025-07-06 15:49:53 +0200

    initial Xcode project

---
+++ Localight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Localight
+//
+//  Created by Timo Köthe on 06.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
